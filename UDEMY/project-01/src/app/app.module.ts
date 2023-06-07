dd495918bce50527f6c5d6c34d148bb492eb644e
Roman Brauer <bulick27kom@gmail.com>
2023-06-07 13:33:14 +0300

    component creating

--- UDEMY/project-01/src/app/app.module.ts
+++ UDEMY/project-01/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingList } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEdit } from './shopping-list/shopping-list-edit/shopping-llist-edit.component';
+import { RecipeListComponent } from './recipe-book/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipes/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingList,
+    ShoppingListEdit,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ UDEMY/project-01/src/app/shopping-list/shopping-list-edit/shopping-llist-edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppingListEdit',
+  templateUrl: './shopping-llist-edit.component.html',
+  styleUrls: ['./shopping-llist-edit.component.css'],
+})
+export class ShoppingListEdit {}
