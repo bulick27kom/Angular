6e5e4f6b2abd3d971850127d371e7d7e41613fa3
Roman Brauer <bulick27kom@gmail.com>
2023-06-09 18:54:57 +0300

    2nd path done

--- UDEMY/project-01/src/app/shopping-list/shopping-list.component.ts
+++ UDEMY/project-01/src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingList {}
+export class ShoppingList {
+  ingredients: Ingredient[] = [
+    new Ingredient('Ingedient 1', 10),
+    new Ingredient('Ingedient 2', 20),
+    new Ingredient('Ingedient 3', 30),
+    new Ingredient('Ingedient 4', 40),
+    new Ingredient('Ingedient 5', 50),
+  ];
+}
