d628d8d952fe07fba5d388ea2b76cd2d64ef8766
Roman Brauer <bulick27kom@gmail.com>
2023-06-15 15:54:48 +0300

    03 practise is done

--- UDEMY/03-game-progect/src/app/even/even.component.ts
+++ UDEMY/03-game-progect/src/app/even/even.component.ts
@@ -6,7 +6,7 @@ import { Component, Input,  } from '@angular/core';
   styleUrls: ['./even.component.css']
 })
 export class EvenComponent {
-  @Input() incomingCounter: number;
+  @Input('evenCounter') counter: number;
 
 }
 
