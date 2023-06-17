ce6163486f0cac2ecfb3580d9b1c16c0b3b7027d
Roman Brauer <bulick27kom@gmail.com>
2023-06-17 16:52:37 +0300

    097 leson

--- UDEMY/04-directives/src/app/app.module.ts
+++ UDEMY/04-directives/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppComponent } from './app.component';
 import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, BasicHighLightDirective],
+  declarations: [
+    AppComponent,
+    BasicHighLightDirective,
+    BetterHighlightDirective,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
